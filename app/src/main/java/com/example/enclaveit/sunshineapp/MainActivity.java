03049d07ad88bb662109468f299e7452fd13c33c
linnguyen <nguyenvanlin01041994@gmail.com>
2016-12-13 17:12:11 +0700

    1/2 task 3

--- app/src/main/java/com/example/enclaveit/sunshineapp/MainActivity.java
+++ app/src/main/java/com/example/enclaveit/sunshineapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.enclaveit.sunshineapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -20,19 +21,31 @@ public class MainActivity extends AppCompatActivity {
         //TODO Create an arrays of string that contain the fake weather data
 
         //Create an array of String that contain fake weather data
-        String [] dummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow, May 17 - Clear - 17°C / 15°C",
-                "Thursday, May 17 - Clear - 17°C / 15°C",
-                "Sunday, May 17 - Clear - 17°C / 15°C",
-                "Tue, May 17 - Clear - 17°C / 15°C",
-                "Wed, May 17 - Clear - 17°C / 15°C"
-        };
+        //delete the dummy weather data. you will be getting the Real data from the Internet in
+        //in this lesson
+//        String [] dummyWeatherData = {
+//                "Today, May 17 - Clear - 17°C / 15°C",
+//                "Tomorrow, May 17 - Clear - 17°C / 15°C",
+//                "Thursday, May 17 - Clear - 17°C / 15°C",
+//                "Sunday, May 17 - Clear - 17°C / 15°C",
+//                "Tue, May 17 - Clear - 17°C / 15°C",
+//                "Wed, May 17 - Clear - 17°C / 15°C"
+//        };
+
+        // Create a class that extends AsyncTask to perform network request
+        //ok
+         public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+             //Override the doIn
+         }
+
 
         // Append each String from the fake weather to the Textview
-        for(String dummyWeatherDay: dummyWeatherData){
-            mWeatherTextView.append(dummyWeatherDay);
-        }
+        // delete the for loop that populates the Textview with dummmy data
+        //ok
+
+//        for(String dummyWeatherDay: dummyWeatherData){
+//            mWeatherTextView.append(dummyWeatherDay);
+//        }
 
     }
 }
